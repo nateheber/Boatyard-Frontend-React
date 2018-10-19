1266bd8194219a00adf73557bce87f2c70bc4284
Daniel Zheng <dannyzheng1993@gmail.com>
2018-10-19 09:11:11 +0800

    mobile Resources

---
+++ src/components/basic/Buttons/Hamburger.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import HamburgerIcon from '../../../resources/left_mobile_nav.svg';
+
+const Wrapper = styled.div`
+  width: 16.66667%;
+  max-width: 195px;
+  display: none;
+  @media (max-width: 991px) {
+    z-index: 99999;
+    display: block !important;
+  }
+`;
+const Button = styled.button`
+  width: 25px;
+  height: 19px;
+  background-color: transparent;
+  border: none;
+  padding: 0;
+  outline: none;
+`;
+
+const Icon = styled.img`
+  width: 25px;
+  height: 19px;
+`;
+
+export const HamburgerButton = props => (
+  <Wrapper>
+    <Button {...props}>
+      <Icon src={HamburgerIcon} />
+    </Button>
+  </Wrapper>
+);
