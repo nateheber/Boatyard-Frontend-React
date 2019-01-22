da619afc96f658b099b0b5199cfa99b4c0b16340
Mykolas Kvieska <mykolaska@outlook.com>
2019-01-22 10:42:46 +0200

    make updates for TableRecord

--- src/components/basic/Table/Record.js
+++ src/components/basic/Table/Record.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 import changeCase from 'change-case';
-import { get } from 'lodash';
 import classNames from 'classnames'
+import moment from 'moment';
 
 const Wrapper = styled.div`
   display: flex;
@@ -32,6 +32,16 @@ const FirstField = styled.div`
   padding: 8px;
   padding-left: 30px;
   align-items: center;
+  > a {
+    text-decoration: none;
+    color: #004258;  
+  }
+  &.is-mobile {
+    display: none;
+  }
+  &.is-desktop {
+    display: flex;
+  }
   @media (max-width: 843px) {
     display: flex;
     height: 57px;
@@ -42,6 +52,12 @@ const FirstField = styled.div`
     border-bottom: 2px solid #e7ecf1 !important;
     &.active {
       background-color: #f6f6f7;
+    }
+    &.is-mobile {
+      display: flex;
+    }
+    &.is-desktop {
+      display: none;
     }
   }
   cursor: pointer;
@@ -81,35 +97,70 @@ export class Record extends React.Component {
   state = {
     show: false
   };
-  onClickFirstRow = () => {
+
+  onShowDetails = () => {
     const { show } = this.state;
     this.setState({ show: !show });
+  };
+
+  onGoToDetails = () => {
     this.props.toDetails();
   };
+
+  getValue = (column, item) => {
+    if (column.value === 'id') {
+      if (item.state === 'draft' && column.type === 'new') {
+        return 'New Order';
+      }
+      return `Order #${item.id}`;    
+    }
+    const fields = column.value.split('/');
+    let value = '';
+    for (const idx in fields) {
+      const field = fields[idx];
+      const arr = field.split('.');
+      let part = item;
+      for (const subIdx in arr) {
+        const key = arr[subIdx];
+        if (!part) return '_';
+        part = part[key];
+      }
+      if(part && part.length > 0) {
+        value = value.length > 0 ? `${value} ${part}` : part;
+      }    
+    }
+    if (column.isValue && parseInt(value) === 0) {
+      return '';
+    }
+    if (column.isDate) {
+      value = `${moment(value).format('MMM DD, YYYY')}`;
+      if (!(value instanceof Date && !isNaN(value))) {
+        value = '';
+      }
+    }
+    return `${column.prefix || ''}${value || '_'}${column.suffix || ''}`;
+  };
+
   render() {
     const { record, columns, type } = this.props;
     const { show } = this.state;
-    const firstField = columns[0].value;
+    const firstColumn = columns[0];
     const hidingCols = columns.slice(1);
     return (
       <Wrapper className={show ? 'active' : 'deactive'}>
         <FirstField
-          onClick={this.onClickFirstRow}
-          className={classNames(show ? 'active' : 'deactive', type)}
+          onClick={this.onShowDetails}
+          className={classNames(show ? 'active' : 'deactive', type, 'is-mobile')}
         >
-          {get(record, firstField) || '_'}
+          {this.getValue(firstColumn, record)}
         </FirstField>
-        {hidingCols.map((col, idx) => (
+        <FirstField className="is-desktop" onClick={this.onGoToDetails}>
+          {this.getValue(firstColumn, record)}
+        </FirstField>
+        {hidingCols.map((column, idx) => (
           <Field className={classNames(show ? 'show' : 'hide', type)} key={`col_${idx}`}>
-            <FieldLabel>{changeCase.upperCaseFirst(col.label)}</FieldLabel>
-            {col.isValue ? 
-              parseInt(get(record, col.value)) > 0 ?
-              <FieldValue>{col.prefix || ''}{get(record, col.value) || '_'}{col.suffix || ''}</FieldValue>
-              :
-              <FieldValue>_</FieldValue>
-            :
-              <FieldValue>{col.prefix || ''}{get(record, col.value) || '_'}{col.suffix || ''}</FieldValue>
-            }
+            <FieldLabel>{changeCase.upperCaseFirst(column.label)}</FieldLabel>
+            <FieldValue>{this.getValue(column, record)}</FieldValue>
           </Field>
         ))}
       </Wrapper>
