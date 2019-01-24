2d4592ac7ce2618d18d7a244b9a4be4cf3379995
Mykolas Kvieska <mykolaska@outlook.com>
2019-01-24 13:52:03 +0200

    Update button style

--- src/components/basic/Buttons/HollowButton.js
+++ src/components/basic/Buttons/HollowButton.js
@@ -16,7 +16,7 @@ export const HollowButton = styled.button`
   text-align: center;
   text-transform: uppercase;
   display: inline-block;
-  height: 33px !important;
+  height: 30px !important;
   margin: 5px;
   padding: 5px 15px;
   outline: none;
