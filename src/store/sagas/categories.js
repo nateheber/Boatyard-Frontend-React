391af10ed4f237998694b46c8a4b05d54e5c694b
Mykolas Kvieska <mykolaska@outlook.com>
2019-02-05 19:00:53 +0200

    Add effects for categories

--- src/store/sagas/categories.js
+++ src/store/sagas/categories.js
@@ -1,61 +1,143 @@
 import { put, takeEvery, call, select } from 'redux-saga/effects';
 import { get } from 'lodash';
 
-import { actions } from '../reducers/categories';
+import { actionTypes } from '../actions/categories';
 import { getCategoryClient } from './sagaSelectors';
 
-function* createRequest(action) {
-  const categoryClient = yield select(getCategoryClient);
-  yield call(categoryClient.create, action.payload);
-  yield put({
-    type: actions.fetchCategories
-  });
-}
-
-function* fetchRequest(action) {
-  const categoryClient = yield select(getCategoryClient);
-  const result = yield call(categoryClient.list);
-  const categories = get(result, 'data', []);
-  yield put({
-    type: actions.setCategories,
-    payload: categories.map(category => ({
+const refineCategories = (categories) => {
+  return categories.map(category => {
+    return {
       id: category.id,
-      ...category.attributes
-    }))
+      ...category.attributes,
+      ...category.relationships
+    };
   });
+};
+
+function* getCategories(action) {
+  const categoryClient = yield select(getCategoryClient);
+  let successType = actionTypes.GET_CATEGORIES_SUCCESS;
+  let failureType = actionTypes.GET_CATEGORIES_FAILURE;
+  const { params, success, error } = action.payload;
+  let result = null;
+  try {
+    result = yield call(categoryClient.list, params);
+    const categories = get(result, 'data', []);
+    const included = get(result, 'included', []);
+    const { perPage, total } = result;
+    switch (action.type) {
+      case actionTypes.FILTER_CATEGORIES: {
+        successType = actionTypes.FILTER_CATEGORIES_SUCCESS;
+        failureType = actionTypes.FILTER_CATEGORIES_FAILURE;
+        break;
+      }
+      default:
+    }
+    const refinedCategories = refineCategories(categories);
+    yield put({
+      type: successType,
+      payload: {
+        categories: refinedCategories,
+        included,
+        perPage,
+        total,
+      }
+    });
+    if (success) {
+      yield call(success, refinedCategories);
+    }
+  } catch (e) {
+    yield put({ type: failureType, payload: e });
+    if (error) {
+      yield call(error);
+    }
+  }
 }
 
-function* deleteRequest(action) {
+function* getCategory(action) {
   const categoryClient = yield select(getCategoryClient);
-  yield call(categoryClient.delete, action.payload);
+  const { categoryId, success, error } = action.payload;
+  let result = null;
+  try {
+    result = yield call(categoryClient.read, categoryId);
+    const { data, included } = result;
+    yield put({
+      type: actionTypes.GET_CATEGORY_SUCCESS,
+      payload: data
+    });
+    if (success) {
+      yield call(success, data, included);
+    }
+  } catch (e) {
+    yield put({ type: actionTypes.GET_CATEGORY_FAILURE, payload: e });
+    if (error) {
+      yield call(error);
+    }
+  }
 }
 
-function* updateRequest(action) {
-  const { id, data } = action.payload;
+function* createCategory(action) {
   const categoryClient = yield select(getCategoryClient);
-  yield call(categoryClient.update, id, data);
-  yield put({
-    type: actions.fetchCategories
-  });
+  const { data, success, error } = action.payload;
+  try {
+    const result = yield call(categoryClient.create, data);
+    yield put({
+      type: actionTypes.CREATE_CATEGORY_SUCCESS,
+    });
+    if (success) {
+      yield call(success, get(result, 'data', {}));
+    }
+  } catch (e) {
+    yield put({ type: actionTypes.CREATE_CATEGORY_FAILURE, payload: e });
+    if (error) {
+      yield call(error);
+    }
+  }
 }
 
-function* selectRequest(action) {
-  const { categoryId, callback } = action.payload;
+function* updateCategory(action) {
   const categoryClient = yield select(getCategoryClient);
-  const result = yield call(categoryClient.read, categoryId);
-  yield put({
-    type: actions.setCategory,
-    payload: result
-  });
-  if (callback) {
-    yield callback(result);
+  const { categoryId, data, success, error } = action.payload;
+  try {
+    yield call(categoryClient.update, categoryId, data);
+    yield put({
+      type: actionTypes.UPDATE_CATEGORY_SUCCESS,
+    });
+    if (success) {
+      yield call(success);
+    }
+  } catch (e) {
+    yield put({ type: actionTypes.UPDATE_CATEGORY_FAILURE, payload: e });
+    if (error) {
+      yield call(error);
+    }
+  }
+}
+
+function* deleteCategory(action) {
+  const categoryClient = yield select(getCategoryClient);
+  const { categoryId, success, error } = action.payload;
+  try {
+    yield call(categoryClient.delete, categoryId);
+    yield put({
+      type: actionTypes.DELETE_CATEGORY_SUCCESS,
+    });
+    if (success) {
+      yield call(success);
+    }
+  } catch (e) {
+    yield put({ type: actionTypes.DELETE_CATEGORY_FAILURE, payload: e });
+    if (error) {
+      yield call(error);
+    }
   }
 }
 
 export default function* Profile() {
-  yield takeEvery(actions.createCategories, createRequest);
-  yield takeEvery(actions.fetchCategories, fetchRequest);
-  yield takeEvery(actions.deleteCategories, deleteRequest);
-  yield takeEvery(actions.updateCategories, updateRequest);
-  yield takeEvery(actions.selectCategory, selectRequest);
+  yield takeEvery(actionTypes.GET_CATEGORIES, getCategories);
+  yield takeEvery(actionTypes.FILTER_CATEGORIES, getCategories);
+  yield takeEvery(actionTypes.GET_CATEGORY, getCategory);
+  yield takeEvery(actionTypes.CREATE_CATEGORY, createCategory);
+  yield takeEvery(actionTypes.DELETE_CATEGORY, deleteCategory);
+  yield takeEvery(actionTypes.UPDATE_CATEGORY, updateCategory);
 }
