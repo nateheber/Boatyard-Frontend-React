b5e62420f7a9a3a9b0636974773af0f35ff41886
Mykolas Kvieska <mykolaska@outlook.com>
2019-02-06 18:44:36 +0200

    Fix type issue

--- src/containers/Services/screens/Services.js
+++ src/containers/Services/screens/Services.js
@@ -52,7 +52,7 @@ class Services extends React.Component {
 
   render() {
     const columns = [
-      { label: 'serivce name', value: 'name', sort: 'name' },
+      { label: 'service name', value: 'name', sort: 'name' },
       { label: 'price', value: 'cost', sort: 'cost', prefix: '$', isValue: true },
       { label: 'price type', value: 'costType', sort: 'cost_type' }
     ];
