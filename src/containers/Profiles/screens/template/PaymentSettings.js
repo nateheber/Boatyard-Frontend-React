c26147135d3b00fb7a89e01689b04375fb1ca51f
Daniel Zheng <dannyzheng1993@gmail.com>
2019-02-08 23:18:05 +0800

    Initial Work for Payment Settings

---
+++ src/containers/Profiles/screens/template/PaymentSettings.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import { OrangeButton, HollowButton } from 'components/basic/Buttons';
+
+class PaymentSettings extends React.Component {
+  render() {
+    return false;
+  }
+}
+
+export default connect(null, null)(PaymentSettings);
