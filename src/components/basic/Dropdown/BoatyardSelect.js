a419066a3917b68ef720a8285bc5dde2096c36f5
Mykolas Kvieska <mykolaska@outlook.com>
2019-02-08 16:37:24 +0200

    Update BoatyardSelect

--- src/components/basic/Dropdown/BoatyardSelect.js
+++ src/components/basic/Dropdown/BoatyardSelect.js
@@ -7,10 +7,12 @@ export const BoatyardSelect = styled(AsyncSelect)`
   font-weight: 400;
   letter-spacing: -0.3px;
   .css-2o5izw {
-    min-height: 35px;
+    min-height: 32px;
+    border-radius: 6px;
   }
   .css-vj8t7z {
-    min-height: 35px;
+    min-height: 32px;
+    border-radius: 6px;
   }
   .css-1wy0on6 {
     .css-1uq0kb5 {
