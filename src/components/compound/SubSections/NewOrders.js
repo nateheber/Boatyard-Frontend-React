61c7eb4c99679d1c3d9425ef04ce0ec17314d748
Mykolas Kvieska <mykolaska@outlook.com>
2019-02-15 18:50:54 +0200

    hide customer info for dispatched order

--- src/components/compound/SubSections/NewOrders.js
+++ src/components/compound/SubSections/NewOrders.js
@@ -2,9 +2,9 @@ import React from 'react';
 import styled from 'styled-components';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-import { get } from 'lodash';
+import { get, merge, orderBy } from 'lodash';
 
-import { GetNewOrders } from 'store/actions/orders';
+import { GetNewOrders, GetOrders } from 'store/actions/orders';
 import { refinedOrdersSelector } from 'store/selectors/orders'
 import { NewOrderSection } from 'components/basic/SubSection';
 import { OrderTable } from 'components/basic/Order';
@@ -20,21 +20,52 @@ const Wrapper = styled.div`
 `;
 
 class NewOrders extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state ={
+      loaded: false,
+      total: 0,
+      orders: []
+    };
+  }
 
   componentDidMount() {
-    this.props.GetNewOrders({
+    const { GetNewOrders, GetOrders } = this.props;
+    GetNewOrders({
       params: {
         page: 1,
         per_page: 5,
         'order[state]': 'draft',
         'order[sort]': 'desc',
         'order[order]': 'created_at'
+      },
+      success: () => {
+        GetOrders({
+          params: {
+            page: 1,
+            per_page: 5,
+            'order[state]': 'dispatched',
+            'order[sort]': 'desc',
+            'order[order]': 'created_at'
+          },
+          success: () => {
+            const { draftTotal, dispatchedTotal, draftOrders, dispatchedOrders } = this.props;
+            let total = draftTotal + dispatchedTotal;
+            let orders = orderBy(merge(draftOrders, dispatchedOrders), ['createdAt'], ['desc']);
+            if (total > 5) {
+              orders = orders.slice(0, 5);
+              total = 5;
+            }
+            this.setState({ loaded: true, total, orders });
+          }
+        });
       }
     });
   }
 
   render() {
-    const { orders, total, history } = this.props;
+    const { history } = this.props;
+    const { loaded, total, orders } = this.state;
     const columns = [
       { label: 'ORDER', value: 'id', isTitle: true, type: 'new', link: true },
       {
@@ -43,36 +74,39 @@ class NewOrders extends React.Component {
           'relationships.user.attributes.firstName/relationships.user.attributes.lastName',
           'relationships.childAccount.attributes.firstName/relationships.childAccount.attributes.lastName'
         ],
-        isCustomer: true
+        isCustomer: true,
+        type: 'new-customer'
       },
       { label: 'SERVICE', value: 'relationships.service.attributes.name' },
       { label: 'BOAT NAME', value: 'relationships.boat.attributes.name' },
       { label: 'BOAT MAKE', value: 'relationships.boat.attributes.make' },
       { label: 'ORDER STATUS', value: 'status' }
     ];
-
     return (
       <Wrapper>
       <NewOrderSection count={total} />
-      <OrderTable
+      {loaded && <OrderTable
         columns={columns}
         items={orders}
-      />
-      <HollowButton className="btn-view-all" onClick={() => history.push('/orders/')}>
+      />}
+      {loaded && <HollowButton className="btn-view-all" onClick={() => history.push('/orders/')}>
         VIEW ALL
-      </HollowButton>
+      </HollowButton>}
     </Wrapper>  
     );
   }
 }
 
 const mapStateToProps = state => ({
-  total: get(state, 'order.newOrders.total'),
-  orders: refinedOrdersSelector(state, 'new')
+  draftTotal: get(state, 'order.newOrders.total'),
+  dispatchedTotal: get(state, 'order.orders.total'),
+  draftOrders: refinedOrdersSelector(state, 'new'),
+  dispatchedOrders: refinedOrdersSelector(state, '')
 });
 
 const mapDispatchToProps = {
-  GetNewOrders
+  GetNewOrders,
+  GetOrders
 };
 
 export default withRouter(connect(
