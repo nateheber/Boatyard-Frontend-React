d891479b7bf507cfec427e6a630f296ec8aa56e1
Mykolas Kvieska <mykolaska@outlook.com>
2019-02-20 15:53:55 +0200

    add search and add button

--- src/components/template/Users.js
+++ src/components/template/Users.js
@@ -1,45 +1,105 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import styled from 'styled-components';
 import { withRouter } from 'react-router-dom';
-
-import Table from 'components/basic/Table';
-import { UsersHeader } from 'components/compound/SectionHeader';
+import styled from 'styled-components';
+import { Row, Col } from 'react-flexbox-grid';
+import { isEmpty } from 'lodash';
 
 import { GetUsers } from 'store/actions/users';
+import Table from 'components/basic/Table';
+import { UsersHeader } from 'components/compound/SectionHeader';
+import { SearchBox } from 'components/basic/Input';
 
 const Wrapper = styled.div`
   height: 100%;
   background-color: white;
 `;
 
+const SearchSection = styled(Row)`
+  border-top: 1px solid #D5DBDE;
+  border-bottom: 1px solid #D5DBDE;
+  margin: 0 0 20px 0 !important;
+`;
+
+const SearchCotainer = styled(Col)`
+  padding: 24px 20px;
+  width: 305px;
+`;
+
+
 class Users extends React.Component {
-  componentDidMount() {
-    this.props.GetUsers({
-      params: { page: 1 }
-    });
+  constructor(props) {
+    super(props);
+    this.state = {
+      keyword: '',
+      sort: { col: '', direction: '' },
+    };
   }
+
+  componentDidMount() {
+    this.loadPage(1);
+  }
+
   onAdd = () => {
     this.props.history.push(`/user-details/`);
   };
+
   toDetails = user => {
     this.props.history.push(`/user-details?user=${user.id}`);
   };
+
+
+  loadPage = (page) => {
+    const { keyword } = this.state;
+    const { GetUsers } = this.props;
+    const { sort } = this.state;
+    const params = isEmpty(keyword) ? {
+      page: page,
+      'user[sort]': sort.direction,
+      'user[order]': sort.col
+    } : {
+      page: page,
+      'user[sort]': sort.direction,
+      'user[order]': sort.col,
+      search_by_full_name: keyword
+    };
+    GetUsers({ params });
+  };
+
+  onSortChange = (sort) => {
+    this.setState({ sort: sort }, () => {
+      this.loadPage(1);
+    });
+  };
+
+  handleInputChange = (keyword) => {
+    this.setState({ keyword }, () => {
+      this.loadPage(1);
+    });
+  };
+
   render() {
     const columns = [
-      { label: 'first name', value: 'firstName' },
-      { label: 'last name', value: 'lastName' },
-      { label: 'email', value: 'email' },
-      { label: 'contact number', value: 'phoneNumber' }
+      { label: 'first name', value: 'firstName', sort: 'first_name' },
+      { label: 'last name', value: 'lastName', sort: 'last_name' },
+      { label: 'email', value: 'email', sort: 'email' },
+      { label: 'contact number', value: 'phoneNumber', sort: 'phone_number' }
     ];
+    const { sort } = this.state;
     const { users } = this.props;
     return (
       <Wrapper>
         <UsersHeader onAdd={this.onAdd} />
+        <SearchSection>
+          <SearchCotainer>
+            <SearchBox placeholder="SEARCH USERS" onChange={this.handleInputChange} />
+          </SearchCotainer>
+        </SearchSection>
         <Table
           columns={columns}
           records={users}
-          sortColumn="order"
+          sort={sort}
+          onSortChange={this.onSortChange}
           toDetails={this.toDetails}
         />
       </Wrapper>
