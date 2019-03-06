aea97fbd72dc8949be18b22c684d19bd0470ada0
Daniel Zheng <dannyzheng1993@gmail.com>
2019-03-06 23:01:19 +0800

    Update for Structure

---
+++ src/containers/Providers/components/LocationEditor/LocationImage/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+import { isEmpty } from 'lodash';
+
+export default class LocationImage extends React.Component {
+  state = {
+    file: ''
+  }
+
+  render () {
+    return false;
+  }
+}
