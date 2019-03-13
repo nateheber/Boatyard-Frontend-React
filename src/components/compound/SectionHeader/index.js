695c3a17aa76d34041da8036f66b9911188d1a23
Mykolas Kvieska <mykolaska@outlook.com>
2019-03-13 21:30:24 +0200

    adding teammember details

--- src/components/compound/SectionHeader/index.js
+++ src/components/compound/SectionHeader/index.js
@@ -2,7 +2,6 @@ export * from './DashboardHeader';
 export * from './OrderHeader';
 export * from './ServiceHeader';
 export * from './UsersHeader';
-export * from './TeamMemberHeader';
 export * from './ProviderHeader';
 export * from './CategoryHeader';
 export * from './BoatHeader';
