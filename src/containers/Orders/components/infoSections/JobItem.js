d45759385fd63e02b16826a15f8e12f3387525f1
Mykolas Kvieska <mykolaska@outlook.com>
2019-05-02 21:06:01 +0300

    add job section

---
+++ src/containers/Orders/components/infoSections/JobItem.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import styled from 'styled-components';
+import { startCase } from 'lodash';
+
+const Wrapper = styled.div`
+  position: relative;
+  color: #8f8f8f;
+  font-family: "Source Sans Pro", sans-serif;
+  font-size: 14px;
+  font-weight: 400;
+  padding: 10px 15px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid #F5F5F5;
+`;
+
+const LeftSection = styled.div`
+`;
+
+const JobTitle = styled.div`
+  font-family: Helvetica;
+  font-size: 16px;
+  color: #003247;
+  text-align: left;
+  line-height: 23px;
+`;
+
+const JobID = styled.div`
+  font-family: Helvetica;
+  font-size: 12px;
+  color: #999999;
+  text-align: left;
+`;
+
+const RightSection = styled.div`
+`;
+
+const Button = styled.button`
+  background: #0D485F;
+  font-family: Helvetica;
+  font-size: 14px;
+  color: #FFFFFF;
+  text-align: center;
+  min-height: 30px;
+  min-width: 133px;
+  border: none;
+  &.accepted {
+    background-color: #F38118;
+  }
+  &.in-progress {
+    background-color: #147397;
+  }
+  &.pending {
+    background-color: #0D485F;
+  }
+  &.completed {
+    background-color: #9F9AA4;
+  }
+  &.declined {
+    background-color: #DE5454;
+  }
+`;
+
+export default class JobItem extends React.Component {
+
+  render () {
+    const { job } = this.props;
+    return (
+      <Wrapper>
+        <LeftSection>
+          <JobTitle>{job.name}</JobTitle>
+          <JobID>{`#${job.id}`}</JobID>
+        </LeftSection>
+        <RightSection>
+          <Button className={job.status}>{startCase(job.status)}</Button>
+        </RightSection>
+      </Wrapper>
+    )
+  }
+}
---
+++ src/containers/Orders/components/templates/JobSection.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+
+import { Section } from 'components/basic/InfoSection';
+import JobItem from '../infoSections/JobItem';
+
+const Wrapper = styled.div`
+  margin: -25px;
+  overflow: auto;
+  max-height: 290px;
+`;
+
+const Button = styled.button`
+  position: relative;
+  width: 30px;
+  height: 30px;
+  padding: 5px;
+  outline: none;
+  cursor: pointer;
+  background: #FFFFFF;
+  background-image: linear-gradient(180deg, rgba(255,255,255,0.12) 0%, rgba(0,0,0,0.12) 100%);
+  border: 1px solid #A9B5BB;
+  border-radius: 6px;
+`;
+
+export default class JobSection extends React.Component {
+  addJob = () => {
+
+  };
+
+  renderAddButton = () => {
+    return (<Button onClick={this.addJob}>
+      <FontAwesomeIcon icon='plus' color='#003247' />
+    </Button>);
+  };
+
+  render () {
+    const jobs = [
+      {
+      id: '23544',
+      name: 'Job Title',
+      status: 'accepted'
+      },
+      {
+      id: '23545',
+      name: 'Medium Job Title',
+      status: 'in-progress'
+      },
+      {
+      id: '23554',
+      name: 'Third Job Title',
+      status: 'pending'
+      },
+      {
+      id: '22344',
+      name: 'Short Job Title',
+      status: 'completed'
+      },
+      {
+      id: '29874',
+      name: 'Really Long Job Title',
+      status: 'declined'
+      },
+      {
+      id: '23544',
+      name: 'Job Title',
+      status: 'accepted'
+      },
+      {
+      id: '23545',
+      name: 'Medium Job Title',
+      status: 'in-progress'
+      },
+      {
+      id: '23554',
+      name: 'Third Job Title',
+      status: 'pending'
+      },
+      {
+      id: '22344',
+      name: 'Short Job Title',
+      status: 'completed'
+      },
+      {
+      id: '29874',
+      name: 'Really Long Job Title',
+      status: 'declined'
+      }
+    ];
+    return (
+      <Section title='Jobs' mode='view' editComponent={this.renderAddButton()}>
+        <Wrapper>
+          {jobs.map((item, idx) => (
+            <JobItem key={`timeline_${idx}`} job={item} />
+          ))}
+        </Wrapper>
+      </Section>
+    )
+  }
+}
