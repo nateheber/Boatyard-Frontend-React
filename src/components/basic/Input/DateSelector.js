8ae861054d63600a7213209d5060f3ea83d0bebf
Mykolas Kvieska <mykolaska@outlook.com>
2019-05-08 19:29:15 +0300

    update date selector

--- src/components/basic/Input/DateSelector.js
+++ src/components/basic/Input/DateSelector.js
@@ -1,14 +1,27 @@
-import React from 'react'
-import DatePicker from 'react-datepicker'
+import React from 'react';
+import DatePicker from 'react-datepicker';
+import styled from 'styled-components';
 
-import "react-datepicker/dist/react-datepicker.css";
-import "./datepicker.css";
+import 'react-datepicker/dist/react-datepicker.css';
+import './datepicker.css';
+
+const DateSelectWrapper = styled.div`
+  width: 100%;
+  .react-datepicker-wrapper {
+    width: 100%;
+    .react-datepicker__input-container {
+      width: 100%;
+    }
+  }
+`;
 
 export class DateSelector extends React.Component {
   render() {
     const { hasError, hideError, errorMessage, ...rest } = this.props;
     return (
-      <DatePicker {...rest} />
+      <DateSelectWrapper>
+        <DatePicker {...rest} />
+      </DateSelectWrapper>
     );
   }
 }
