ad81a40764af5172a818e3b904b133a20c0c3f82
Mykolas Kvieska <mykolaska@outlook.com>
2019-05-08 21:28:24 +0300

    update DateSelector

--- src/components/basic/Input/DateSelector.js
+++ src/components/basic/Input/DateSelector.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import DatePicker from 'react-datepicker';
 import styled from 'styled-components';
+import classNames from 'classnames';
 
 import 'react-datepicker/dist/react-datepicker.css';
 import './datepicker.css';
@@ -11,7 +12,26 @@ const DateSelectWrapper = styled.div`
     width: 100%;
     .react-datepicker__input-container {
       width: 100%;
+      >input {
+        margin-bottom: 5px;
+      }
     }
+  }
+`;
+
+const ErrorMessage = styled.div`
+  color: #f7941e !important;
+  display: block;
+  font-weight: 400;
+  font-size: 12px;
+  margin: 0 0 5px;
+  font-family: 'Source Sans', sans-serif;
+  line-height: 1.125;
+  text-transform: capitalize;
+  opacity: 0;
+  transition: opacity 0.5s;
+  &.show {
+    opacity: 1;
   }
 `;
 
@@ -21,6 +41,11 @@ export class DateSelector extends React.Component {
     return (
       <DateSelectWrapper>
         <DatePicker {...rest} />
+        { (!hideError && hasError) &&
+          <ErrorMessage className={classNames({ show: hasError })}>
+            {errorMessage}
+          </ErrorMessage>
+        }
       </DateSelectWrapper>
     );
   }
