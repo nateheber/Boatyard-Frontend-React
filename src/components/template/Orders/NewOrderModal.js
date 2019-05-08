fa50090fee85330ba02bffa2e9e69cef3f5294bb
Mykolas Kvieska <mykolaska@outlook.com>
2019-05-08 21:00:42 +0300

    update create order

--- src/components/template/Orders/NewOrderModal.js
+++ src/components/template/Orders/NewOrderModal.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { get } from 'lodash';
+import moment from 'moment';
 
 import { CreateOrder, UpdateOrder } from 'store/actions/orders';
 import SelectCustomerModal from './SelectCustomerModal';
 import SelectServiceModal from './SelectServiceModal';
+import * as constants from 'utils/constants';
 
 class NewOrderModal extends React.Component {
   constructor(props) {
@@ -15,7 +17,6 @@ class NewOrderModal extends React.Component {
       customer: -1,
       boat: -1
     };
-  
   }
 
   createOrder = () => {
@@ -48,7 +49,8 @@ class NewOrderModal extends React.Component {
     })
   };
 
-  createNewOrder = (service, serviceValues = {}, orderValues = {}) => {
+  createNewOrder = (service, whenValues = {}, serviceValues = {}, orderValues = {}) => {
+    console.log('----------3--------');
     const { CreateOrder, UpdateOrder, onFinishCreation, privilege } = this.props;
     const { customer, boat } = this.state;
     const orderData = {
@@ -64,6 +66,52 @@ class NewOrderModal extends React.Component {
       //   ...serviceValues
       // }
     };
+    console.log('----------4--------');
+    if (whenValues.hasOwnProperty('when')) {
+      const scheduleAttributes = {
+        asap: null,
+        flexible: null,
+        complicated: null,
+        specific_start: null,
+        specific_end: null
+      };
+  
+      switch (whenValues['when']) {
+        case constants.WHEN_FLEXIBLE_OPTION: {
+          scheduleAttributes['flexible'] = true;
+          break;
+        }
+        case constants.WHEN_ASAP_OPTION: {
+          scheduleAttributes['asap'] = true;
+          break;
+        }
+        case constants.WHEN_SPEICFIC_DATE_OPTION: {
+          if (whenValues.hasOwnProperty('day') && whenValues.hasOwnProperty('slot')) {
+            const day = whenValues['day'];
+            const slot = whenValues['slot'];
+            let specific_start = moment(day);
+            let specific_end = moment(day);
+            if (slot === constants.WHEN_SLOT_MORNING) {
+              specific_start = specific_start.set({ hour: 0, minute: 0, second: 0, millisecond: 0 });
+              specific_end = specific_end.set({ hour: 12, minute: 0, second: 0, millisecond: 0 });
+            } else {
+              specific_start = specific_start.set({ hour: 12, minute: 0,second: 0, millisecond: 0 });
+              specific_end = specific_end.set({ hour: 23, minute: 59, second: 59, millisecond: 999 });
+            }
+            scheduleAttributes['specific_start'] = specific_start.format('YYYY-MM-DD HH:mm:ssZ');
+            scheduleAttributes['specific_end'] = specific_end.format('YYYY-MM-DD HH:mm:ssZ');
+          }
+          break;
+        }
+        case constants.WHEN_COMPLICATED_OPTION: {
+          scheduleAttributes['complicated'] = true;
+          break;
+        }
+        default:
+          break;
+      }
+      orderData['line_items_attributes'][0]['line_item_schedules_attributes'] = [scheduleAttributes];
+    }
     if (privilege === 'admin') {
       orderData['user_id'] = customer.id;
     } else {
