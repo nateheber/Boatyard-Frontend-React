16d72eea63a7c12a0ad0a76a06b6d61bc7a8ce01
Fabiano Frizzo <ffrizzo@gmail.com>
2019-05-13 19:17:09 -0300

    Update deploy script to make request to webhook to deploy on ec2

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -20,3 +20,5 @@ aws s3 sync \
 aws cloudfront create-invalidation \
   --distribution-id $CLOUDFRONT_DIST_ID \
   --paths /*
+
+curl -X GET -H 'Token: $WEBHOOK_TOKEN' http://secret-$NODE_ENV.boatyard.com/hooks/travis
