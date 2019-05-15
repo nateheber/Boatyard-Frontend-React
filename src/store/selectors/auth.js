4d8d227f1094c9464a560b5f76101672edf1265a
Mykolas Kvieska <mykolaska@outlook.com>
2019-05-15 14:53:41 +0300

    add auth selector

--- src/store/selectors/auth.js
+++ src/store/selectors/auth.js
@@ -1,5 +1,5 @@
 import { get, isEmpty } from 'lodash';
 
 export const authSelector = state => get(state, 'auth');
-export const isAuthenticatedSelector = state => isEmpty(get(state, 'auth.adminToken')) && isEmpty(get(state, 'auth.providerToken'));
+export const isAuthenticatedSelector = state => !isEmpty(get(state, 'auth.adminToken')) || !isEmpty(get(state, 'auth.providerToken'));
 export const getPrevilage = state => get(state, 'auth.privilege');
