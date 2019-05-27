26dbb8996eb0b4e4458bb27e25d28607b65f3fd6
Mykolas Kvieska <mykolaska@outlook.com>
2019-05-27 16:22:46 +0300

    update ui for admin categories

--- src/containers/Providers/components/AppEditor/components/AppServiceCategories/components/ServiceCategorySelector/index.js
+++ src/containers/Providers/components/AppEditor/components/AppServiceCategories/components/ServiceCategorySelector/index.js
@@ -17,9 +17,10 @@ const Tile = styled.div`
     display: flex;
     background: #F8F8F8;
     align-items: center;
-    padding: 20px 10px 20px 30px;
+    padding: 0px 10px 0px 30px;
     cursor: pointer;
     border-radius: 6px;
+    height: 78px;
     &:hover {
       background: #EEE;
     }
