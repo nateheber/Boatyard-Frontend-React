a6eed57664d3c052a21e8fae165b0a57ef2354c0
Mykolas Kvieska <mykolaska@outlook.com>
2019-05-31 18:58:48 +0300

    add config for intercom

--- src/api/config.js
+++ src/api/config.js
@@ -15,3 +15,4 @@ switch(mode) {
 export const apiBaseUrl = apiUrl;
 export const spreedlyApiUrl = 'https://core.spreedly.com/v1/payment_methods.json?environment_key=7LAxQHurhgD3H3bqnU3XTtXjXlt';
 export const spreedlyApiToken = '3Xp9txPFczmh4YA7PXaO4QOmShmjaNaRyIp44d8Ya347uxS8O3xH3NfPG2KSP7wk=';
+export const intercomAppId = 'oshrxea5';
