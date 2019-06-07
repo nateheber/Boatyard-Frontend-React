943e6376356fbf49586d00f3ad89d9ff0b03055c
Mykolas Kvieska <mykolaska@outlook.com>
2019-06-07 16:04:14 +0300

    Add inputable selector

--- src/components/basic/Input/Selector.js
+++ src/components/basic/Input/Selector.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Select from 'react-select';
 import styled from 'styled-components';
+import classNames from 'classnames';
 
 const Option = styled.div`
   color: #337ab7;
@@ -15,7 +16,23 @@ const Value = styled.div`
   overflow: hidden;
   white-space: nowrap;
   text-overflow: ellipsis;
-`
+`;
+
+const ErrorMessage = styled.div`
+  color: #f7941e !important;
+  display: block;
+  font-weight: 400;
+  font-size: 12px;
+  margin: 0 0 5px;
+  font-family: 'Source Sans', sans-serif;
+  line-height: 1.125;
+  text-transform: capitalize;
+  opacity: 0;
+  transition: opacity 0.5s;
+  &.show {
+    opacity: 1;
+  }
+`;
 
 const StyledSelector = styled(Select)`
   font-size: 13px;
@@ -62,6 +79,76 @@ export class Selector extends React.Component {
         }}
         {...this.props}
       />
+    );
+  }
+}
+
+
+const colourStyles = {
+  container: styles => ({
+    ...styles,
+    minWidth: 100
+  }),
+  control: styles => ({
+    ...styles,
+    backgroundColor: 'white',
+    fontSize: 14,
+    fontFamily: 'Source Sans Pro, sans-serif',
+    fontWeight: 400,
+    letterSpacing: -0.3,
+    minHeight: 28,
+    border: '1px solid #dfdfdf'
+  }),
+  input: styles => ({
+    ...styles,
+    fontFamily: 'Source Sans Pro, sans-serif',
+    fontWeight: 200,
+    fontSize: 14,
+    color: '#003247',
+    paddingTop: 1,
+    paddingBottom: 1
+  }),
+  dropdownIndicator: styles => ({
+    ...styles,
+    display: 'none'
+  }),
+  indicatorSeparator: styles => ({
+    ...styles,
+    display: 'none'
+  }),
+  clearIndicator: styles => ({
+    ...styles,
+    display: 'none'
+  }),
+  singleValue: styles => ({
+    ...styles,
+    fontFamily: 'Source Sans Pro, sans-serif',
+    fontSize: 14,
+    color: '#003247',
+    textTransform: 'initial'
+  }),
+  option: styles => ({
+    ...styles,
+    fontFamily: 'Source Sans Pro, sans-serif',
+    fontSize: 14,
+    color: '#003247',
+    textTransform: 'initial'
+  }),
+  placeholder: styles => ({ ...styles }),
+};
+
+export class InputableSelect extends React.Component {
+  render() {
+    const { hasError, errorMessage, hideError, ...rest } = this.props;
+    return (
+      <React.Fragment>
+        <Select {...rest} styles={colourStyles} />
+        { (!hideError && hasError) &&
+          <ErrorMessage className={classNames({ show: hasError })}>
+            {errorMessage}
+          </ErrorMessage>
+        }
+      </React.Fragment>
     );
   }
 }
