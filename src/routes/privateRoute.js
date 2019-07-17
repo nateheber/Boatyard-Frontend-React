8a1b9bb540ab0a35f6ff94ff975c30f5d742d4fd
Mykolas Kvieska <mykolaska@outlook.com>
2019-07-18 05:48:55 +0800

    hotfix for reload orders when location is changed

--- src/routes/privateRoute.js
+++ src/routes/privateRoute.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import MainPageTemplate from 'components/template/MainPageTemplate';
 import { Route, Redirect } from 'react-router-dom';
 const PrivateRoute = ({ isAuthenticated, component: Component, ...rest }) => {
   console.log('privateroute....');
   return (
   <Route {...rest} render={(props) => (
-    isAuthenticated ?   
-      (
-        <MainPageTemplate>
-          <Component {...props} />
-        </MainPageTemplate>
-      ) :
+    isAuthenticated ? <Component {...props} />:
       <Redirect to="/login" />
   )} />
 )};
