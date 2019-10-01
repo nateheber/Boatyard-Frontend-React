1db64e08d1f672e057ac8a0f1966818f645a504d
Mykolas Kvieska <mykolaska@outlook.com>
2019-10-01 15:43:46 +0300

    update HeaderWrapper styles

--- src/components/basic/Header/HeaderWrapper.js
+++ src/components/basic/Header/HeaderWrapper.js
@@ -6,13 +6,13 @@ const Wrapper = styled(Row)`
   position: fixed;
   left: 0;
   margin: 0px !important;
-  height: 68px;
   width: 100%;
   background-color: #0D485F;
-  align-items: center;
   z-index: 1001;
   box-sizing: border-box;
+  display: flex;
   align-items: center;
+  justify-content: space-between;
 `;
 
 export const HeaderWrapper = props => <Wrapper>{props.children}</Wrapper>;
