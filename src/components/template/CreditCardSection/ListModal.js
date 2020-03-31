5456a18df868d8aaf83624e3f653d3716e772b18
Lawrence <lawrence@bookshout.com>
2020-03-31 17:31:46 -0500

    order cc by createdAt

--- src/components/template/CreditCardSection/ListModal.js
+++ src/components/template/CreditCardSection/ListModal.js
@@ -48,7 +48,8 @@ class ListModal extends React.Component {
 
   render() {
     const { open, onClose, creditCards, onNew, currentStatus } = this.props;
-    const orderCards = orderBy(creditCards, ['attributes.isDefault'], ['desc']);
+    const orderCards = orderBy(creditCards, ['attributes.createdAt'], ['desc']);
+    console.log(orderCards);
     return (
       <Modal
         title="Payment Methods"
